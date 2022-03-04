b3fe714b4429ffcaf3265db0d438a0c3dd83283d
Ariel Tynan <tynan.a@northeastern.edu>
2022-03-04 15:33:31 -0500

    Solved on 4March2022

--- Euler_045_Py.py
+++ Euler_045_Py.py
@@ -1,1 +1,23 @@
+# Ariel Tynan
+# Euler Problem_045 Triangular, pentagonal, and hexagonal
+# Started and Solved 4March2022
 
+
+from numpy import sqrt 
+
+hexNums = []
+n = 100000 #number of hexNums analyzed
+
+for i in range(1,n):
+    pentCheck = 0.5*(sqrt(8*(i*(2*i - 1)) + 1) - 1) 
+    triCheck = 1/6*(1 + sqrt(24*(i*(2*i - 1)) + 1))
+    
+    if triCheck.is_integer() and pentCheck.is_integer(): #hex/pent, hex/tri
+        print(i, 2*i*i - i)
+
+#Output gives next valid number, which is answer      
+
+
+# Strategy
+# Make array of hex numbers, they scale the fastest
+# Check if each number is triangle and pentagonal
